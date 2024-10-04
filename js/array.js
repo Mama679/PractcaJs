c2d8efdb40509d94302630974c5884c21609723b
= <=>
2024-10-03 20:15:25 -0500

    Agregar funciones de mostrar items y eliminar

--- js/array.js
+++ js/array.js
@@ -1,13 +1,33 @@
-//Swal.fire("SweetAlert2 is working!");
+//Obtenemos información almacenada en el localStorage
 var items = localStorage.getItem('itemList');
 
+//Convertimos los datos items(viene en cadena) en tipo array 
 items = items ? items.split(','):[];
 
 function addItem(){
    let item = document.getElementById('nameItem').value;
    if(item){
     items.push(item);
-    item.value = "";
+    showItem();
    }
+   
+}
+
+function showItem(){
+   document.getElementById('nameItem').value = '';
+
+   let html = "";
+   for(let i = 0; i < items.length; i++){
+      html += "<div class='col-6  m-2'>"+ items[i] + "</div>";
+      html += `<div class="col-2 m-2"><button class="btn btn-danger" onClick="deleteItem(${i})">X</button></div>`;
+   }
+
+   //Guardar datos de la clave Itemlist en el localStorage
    localStorage.setItem('itemList', items);
+   document.getElementById('listItems').innerHTML = html;
+}
+
+function deleteItem(item){
+   items.splice(item, 1);
+   showItem();
 }
